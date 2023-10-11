6112a2f47532a34a6b1a11608f4a78328b58493c
jesson-stylesheet <145638928+jesson-stylesheet@users.noreply.github.com>
2023-10-11 16:41:39 +0800

    Updating via desktop app

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Initialize Supabase client (replace with your own Supabase URL and public anon key)
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
 const supabase = createClient('https://qqilepmtenqjiivhsjhe.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxaWxlcG10ZW5xamlpdmhzamhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1NzcyMzcsImV4cCI6MjAxMjE1MzIzN30.P-R8tBy9jCedYMbcWyn2agEGWTDkD9Owmdz6LV8qnoc');
 
 async function verifyEmail() {
